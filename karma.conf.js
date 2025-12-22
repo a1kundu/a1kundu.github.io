5cd24dbee7b99e9f5fb6bb692daca054a70de804
ARIJIT KUNDU <arikundu9@gmail.com>
2025-12-22 09:56:33 +0530

    fix: update project name and output paths for consistency

--- karma.conf.js
+++ karma.conf.js
@@ -19,7 +19,7 @@ module.exports = function (config) {
             suppressAll: true, // removes the duplicated traces
         },
         coverageReporter: {
-            dir: require("path").join(__dirname, "./coverage/ngtail-base"),
+            dir: require("path").join(__dirname, "./coverage/a1kundu.github.io"),
             subdir: ".",
             reporters: [{ type: "html" }, { type: "text-summary" }],
         },
